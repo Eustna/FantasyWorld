46d5d731347bd91f732a71cdcec18a103fd1fc53
Eustna <1786939115@qq.com>
2024-02-07 18:01:27 +0800

    update

---
+++ HistorySetting/Yealsor/list.py
@@ -0,0 +1,199 @@
+if True:#1.1文明
+    Limdis = (
+        "1.1.1",
+        "Limdis",
+        "Yealsor里面南方地区主要的文明"
+    )
+
+    Aeray = (
+        "1.1.2",
+        "Aeray",
+        "Limdis文明中一条分支，由逃亡者所建立"
+    )
+
+    Orddnisy = (
+        "1.1.3",
+        "Orddnisy",
+        "Yealsor中北方地区的主要文明"
+    )
+
+    Aediu = (
+        "1.1.4",
+        "Aediu",
+        "Limdis文明中另一条分支"
+    )
+if True:#1.2城邦
+    Xiuna = (
+        "1.2.1",
+        "Xiuna",
+        "Limdis文明中最著名的君主制城邦"
+    )
+
+    Zipor = (
+        "1.2.2",
+        "Zipor",
+        "Limdis文明中著名的共和制城邦"
+    )
+
+    Firom = (
+        "1.2.3",
+        "Firom",
+        ""
+    )
+
+    Sadrce = (
+        "1.2.4",
+        "Sadrce",
+        ""
+    )
+
+    Fudge = (
+        "1.2.5",
+        "Fidge",
+        ""
+    )
+
+    Lapuse = (
+        "1.2.6",
+        "Lapuse",
+        ""
+    )
+#可用名字还有Avalon Briarwood Cinderfall Duskendale Everlight Frosthold Glimmerdeep Havencrest Ironspire Jadeport Kingspire Lionoar Mysthaven Nightsong Oakenfort Pinevale Quartzhall Ravenrock Stormwatch Thundertop
+
+if True:#1.3地区
+    Queto = (
+        "1.3.1",
+        "Queto",
+        "Limdis文明中主要地区",
+        "平原"
+    )
+
+    Euro =(
+        "1.3.2",
+        "Euro",
+        "暂无",
+        "地区"
+    )
+
+    NxRoulise =(
+        "1.3.3",
+        "NxRoulise",
+        "隔绝Yider大陆的一条山脉",
+        "山脉"
+    )
+
+    Silder = (
+        "1.3.4",
+        "Silder",
+        "Orddnisy文明所处的地区，北面为大海，南面为Erosa山脉",
+        "平原"
+    )
+
+    Erosa = (
+        "1.3.5",
+        "Erosa",
+        "隔绝Limdis文明和Orddnisy的一条山脉",
+        "山脉"
+    )
+
+    Namid = (
+        "1.3.6",
+        "Namid",
+        "Queto平原上的那个大湖",
+        "湖泊"
+    )
+
+    Garlum = (
+        "1.3.7",
+        "Garlum",
+        "Xiuna所处的地区",
+        "沼泽"
+    )
+
+    Pukral = (
+        "1.3.8",
+        "Pukral",
+        "Queto平原上一条河流",
+        "河流"
+    )
+
+    Drou = (
+        "1.3.9",
+        "Drou",
+        "Pukral的东侧， Aediu人生活的地区",
+        "平原"
+    )
+#可用地名Frostwind Emerald Silverwood Crystal Sunfire Moonshadow Starfall Stormwatch Twilight Dawnbreak Skyreach Nightwhisper Windhaven Starlight Thunderpeak Rivermist Shadowglen Goldenleaf Firestone Icetide
+
+if True:#1.4人名，0为名，3为姓或族，也就是次名，其后为其他，比如字啥的
+    Midura = (
+        "1.4.1",
+        "Midura-Xiuna-Arisau",
+        "暂无",
+        "Midura",
+        "Arisau",
+        "Xiuna"
+    )
+
+    Ielous = (
+        "1.4.2",
+        "Ielous-Xiuna-Farane",
+        "Xiuna城邦中僭主改君主里面的主要人物",
+        "Ielous",
+        "Farane"
+    )
+    
+    Magee = (
+        "1.4.3",
+        "Magee-Xiuna-Farane",
+        "Farane-Ielous之子，葬仪之乱对的主要人物",
+        "Magee",
+        "Farane"
+    )
+
+    Dugela = (
+        "1.4.4",
+        "Bran-Dugela",
+        "Xiuna的传说人物，是一位英雄，斩杀了巨蛇",
+        "Dugela",
+        "Bran"
+    )
+#可使用的人物Aeris  Caelum Dara Elysia Fenris Galen Halia Irisa Jareth Kael Liora Maelis Nara Oren Paelen Quinn Raelin Soren Talia
+
+if True:#1.5其他
+    Dela = (
+        "1.5.1",
+        "Dela",
+        "Xiuna城邦中早期一种兵役制度",
+        "从兵"
+    )
+
+    Wolunaama = (
+        "1.5.2",
+        "Wolunaama",
+        "Xiuna城邦中一种官职",
+        "武库官"
+    )
+
+    Xipinous = (
+        "1.5.3",
+        "Xipinous",
+        "Xiuna神话中的主神",
+        "主神"
+    )
+
+    Kalar = (
+        "1.5.4",
+        "Kalar",
+        "Limdis文明中一首英雄史诗",
+        "长诗"
+    )
+
+if True:#1.6 事件
+    Humulu = (
+        "1.6.1",
+        "Humulu",
+        "暂无"
+        "战争"
+    )
+
